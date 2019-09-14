e6b06b18c9599899258374b41726b8efa2930c4a
douglas.paz <polarCore>
2019-09-14 18:25:30 -0300

    setup

--- src/main/kotlin/br/com/pucrs/hackaton/config/FirestoreConfig.kt
+++ src/main/kotlin/br/com/pucrs/hackaton/config/FirestoreConfig.kt
@@ -1,13 +1,14 @@
 package br.com.pucrs.hackaton.config
 
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import com.google.firebase.cloud.FirestoreClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 
-import java.io.FileInputStream
 import java.io.IOException
 
 @Configuration
@@ -15,13 +16,14 @@ class FirestoreConfig {
 
     @Bean
     @Throws(IOException::class)
-    fun firestore() {
+    fun firestore(): Firestore {
+        val inputStream = ClassPathResource("/hackatonadesastres-firebase-adminsdk-emcbo-e22c507f0d.json").inputStream
         val options = FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(ClassPathResource("/hackatonadesastres-firebase-adminsdk-emcbo-e22c507f0d.json").inputStream))
+                .setCredentials(GoogleCredentials.fromStream(inputStream))
                 .setDatabaseUrl("https://hackatonadesastres.firebaseio.com")
                 .build()
 
         FirebaseApp.initializeApp(options)
+        return FirestoreClient.getFirestore()
     }
-
 }
